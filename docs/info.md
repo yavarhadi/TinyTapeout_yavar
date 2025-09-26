da9a892c1bdf339c7f4673fa90ce962913fcc209
yavarhadi <yvr.hadi@gmail.com.com>
2025-09-26 14:13:47 +0200

    tb.v update

--- docs/info.md
+++ docs/info.md
@@ -9,11 +9,11 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Explain how your project works--> asimple counter
 
 ## How to test
 
-Explain how to use your project
+Explain how to use your project--> with tb_counter
 
 ## External hardware
 
--- test/tb.v
+++ test/tb.v
@@ -1,6 +1,9 @@
-`timescale 1ns/1ps
+`timescale 1ns/ps
 `default_nettype none
+<<<<<<< HEAD
 `include "src/tt_um_counter.v"
+=======
+>>>>>>> d25bfd5 (tb.v update)
 module tb_counter;
   localparam integer BW =3;
 
@@ -37,6 +40,29 @@ module tb_counter;
     end
     $finish;
   end
+
+
+
+
+
+   // Replace tt_um_example with your module name:
+  tt_um_counter (
+
+      // Include power ports for the Gate Level test:
+`ifdef GL_TEST
+      .VPWR(VPWR),
+      .VGND(VGND),
+`endif
+
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 endmodule
 
 `default_nettype wire
