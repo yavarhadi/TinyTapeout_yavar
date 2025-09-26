069bd29e0e7205cccdbd0e3a6810ce2e4fb366aa
yavarhadi <yvr.hadi@gmail.com.com>
2025-09-26 19:55:39 +0200

    update counter_tb

--- test/counter_tb.v
+++ test/counter_tb.v
@@ -1,9 +1,5 @@
-`timescale 1ns/ps
+`timescale 1ns/1ps
 `default_nettype none
-<<<<<<< HEAD
-`include "src/tt_um_counter.v"
-=======
->>>>>>> d25bfd5 (tb.v update)
 module counter_tb;
   localparam integer BW =3;
 
@@ -11,8 +7,9 @@ module counter_tb;
   reg                   rst;
   wire [BW-1:0]         q;
 
+
   // DUT
-  counter #(BW) dut (
+  tt_um_counter #(BW) dut (
     .clk_i(clk),
     .rst_i(rst),
     .counter_val_o(q)
@@ -25,8 +22,8 @@ module counter_tb;
   integer i;
   initial begin
     // waves
-    $dumpfile("tb_counter.vcd");
-    $dumpvars(0, tb_counter);
+    $dumpfile("counter_tb.vcd");
+    $dumpvars(0, counter_tb);
 
     // reset 3 cycles
     rst = 1'b1;
@@ -45,25 +42,7 @@ module counter_tb;
 
 
 
-   // Replace tt_um_example with your module name:
-  tt_um_counter user_project(
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
 endmodule
 
 `default_nettype wire
-
